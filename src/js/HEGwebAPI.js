8c08a3246b3ae041e8fa24ec919e868e27425b1b
Bubba Skrimp <brewster.joshua1@gmail.com>
2020-10-25 14:40:11 -0800

    fixityfix

--- src/js/HEGwebAPI.js
+++ src/js/HEGwebAPI.js
@@ -2536,17 +2536,17 @@ export class bleUtils { //This is formatted for the way the HEG sends/receives i
       .then(sleeper(100)).then(service => { 
         this.service = service;
         if(this.android == true){
-          service.getCharaacteristic(rxUUID).then(sleeper(100)).then(tx => {
+          service.getCharacteristic(rxUUID).then(sleeper(100)).then(tx => {
             return tx.writeValue(this.encoder.encode("o")); // Fast output mode for android
           });
         }
-          service.getCharacteristic(rxUUID).then(sleeper(400)).then(tx => {
+          service.getCharacteristic(rxUUID).then(sleeper(100)).then(tx => {
             this.rxchar = tx;
             return tx.writeValue(this.encoder.encode("t")); // Send command to start HEG automatically (if not already started)
           });
           return service.getCharacteristic(txUUID) // Get stream source
       })
-      .then(sleeper(500)).then(characteristic=>{
+      .then(sleeper(100)).then(characteristic=>{
           this.txchar = characteristic;
           return characteristic.startNotifications(); // Subscribe to stream
       })
